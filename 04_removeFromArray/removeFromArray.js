d0201fb5ce99c16c199d85f05238684cb72e0639
EHoldener <eholdener12@gmail.com>
2022-03-02 22:48:41 -0800

    passed all tests on 04 - remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+    let arrayToCheck = args[0];
+    args.shift();
+
+    let spliceIndexes = [];
+
+    for (let i=0; i<arrayToCheck.length; i++) {
+        for (let j=0; j<args.length; j++) {
+            if (arrayToCheck[i] === args[j]) {
+                spliceIndexes.push(i);
+            }
+        }
+    }
+
+    spliceIndexes.sort();
+
+    for (let k = spliceIndexes.length - 1; k>=0; k--) {
+        arrayToCheck.splice(spliceIndexes[k],1);
+    }
+    
+    return arrayToCheck;
 
 };
 
