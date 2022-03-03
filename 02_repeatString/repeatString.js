6edf5ddeb570f4927787cad91cd2683aea45c9d2
EHoldener <eholdener12@gmail.com>
2022-03-02 22:07:34 -0800

    passed all tests in 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let repeatedString = ''
+    if (times < 0) {
+        return 'ERROR';
+    }
+    
+    for (i=0; i<times; i++) {
+        repeatedString = repeatedString + string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
