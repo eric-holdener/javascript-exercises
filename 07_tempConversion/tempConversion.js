ca69a850ae22ee4828ef38c66949df2878015912
EHoldener <eholdener12@gmail.com>
2022-03-02 23:06:07 -0800

    passd all tests in 07 - temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+  let tempCelsius = (tempFahrenheit-32) * (5/9);
+  let stringRoundedTempCelsius = tempCelsius.toFixed(1);
+  let roundedTempCelsius = parseFloat(stringRoundedTempCelsius);
+  return roundedTempCelsius;
 };
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+  let tempFahrenheit = tempCelsius * (9/5) + 32;
+  let stringRoundedTempFahrenheit = tempFahrenheit.toFixed(1);
+  let roundedTempFahrenheit = parseFloat(stringRoundedTempFahrenheit);
+  return roundedTempFahrenheit;
 };
 
 // Do not edit below this line
