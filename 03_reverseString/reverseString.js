dd360d2b0f4145c32d11a828f7f4ef705aa43e19
EHoldener <eholdener12@gmail.com>
2022-03-02 22:11:21 -0800

    passed all tests in 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var splitString = string.split("");
 
+    var reverseString = splitString.reverse();
+
+    var reversedWord = reverseString.join("");
+
+    return reversedWord;
 };
 
 // Do not edit below this line
