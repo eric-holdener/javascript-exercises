6a51338a2f1d595e2555103750331c4aceafb70c
EHoldener <eholdener12@gmail.com>
2022-03-08 10:20:28 -0800

    passed all tests in find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
 
+    const ordered = people.sort((firstPerson, secondPerson) => 
+        (firstPerson.yearOfDeath - firstPerson.yearOfBirth) >
+        (secondPerson.yearOfDeath - secondPerson.yearOfBirth) ? -1 : 1);
+
+    return ordered[0];
 };
 
 // Do not edit below this line
