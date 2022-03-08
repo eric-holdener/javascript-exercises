087c61da0af3540d5f23b9081b4b5535a1113007
EHoldener <eholdener12@gmail.com>
2022-03-08 10:06:52 -0800

    passed all tests for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let fibonacciNum = 1;
+    let currentNum = 1;
+    let pastNum = 0;
 
+    if (number < 0) {
+        return 'OOPS';
+    } else if (typeof number === 'string') {
+        number = parseInt(number);
+        for (let i = 0; i <= number - 2; i++) {
+            fibonacciNum = currentNum + pastNum;
+            pastNum = currentNum;
+            currentNum = fibonacciNum;
+        }
+    } else {
+        for (let i = 0; i <= number -2; i++) {
+            fibonacciNum = currentNum + pastNum;
+            pastNum = currentNum;
+            currentNum = fibonacciNum;
+        }
+    }
+    return fibonacciNum;
 };
 
 // Do not edit below this line
