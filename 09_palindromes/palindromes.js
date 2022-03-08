09ed915f14cc163ad5032781cc5a301414c40a1a
EHoldener <eholdener12@gmail.com>
2022-03-08 09:44:03 -0800

    passed all tests in palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+    let splitString = string.split("");
+
+    let cleanLetters = splitString.filter(letter => 
+        punctuation.indexOf(letter) === -1);
+
+    string = cleanLetters.join("");
+    string = string.replace(/\s/g, '');
+
+    let reverseArray = cleanLetters.reverse();
+    let reversedString = reverseArray.join("");
+    reversedString = reversedString.replace(/\s/g, '');
+
+    if (string.toLowerCase() === reversedString.toLowerCase()){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
