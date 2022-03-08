916597f1cdba393ac6166d5378f67045890ac03f
EHoldener <eholdener12@gmail.com>
2022-03-08 09:33:34 -0800

    passed all calculator tests, got stuck for a while trying to solve sum in add which was confusing me until I realized there were two different functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,9 +1,12 @@
 const add = function() {
-  const numbers = [];
-	for (let i = 0; i < arguments.length; i++) {
-    numbers.push(arguments[i]);
+  let numbers = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+  numbers.push(arguments[i]);
   }
-  const sum = numbers.reduce((partialSum, a) => partialSum + a, 0);
+  const sum = numbers.reduce((previousValue, currentValue) => 
+    previousValue + currentValue, 0);
+  
   return sum;
 };
 
@@ -14,35 +17,39 @@ const subtract = function() {
   }
 
   const subtract = numbers.reduce((a, b) => a - b);
+
   return subtract;
 };
 
 const sum = function() {
-  const numbers = [];
-	for (let i = 0; i < arguments.length; i++) {
-    numbers.push(arguments[i]);
-  }
+  const numbers = arguments[0];
+  const sum = numbers.reduce((previousValue, currentValue) => 
+    previousValue + currentValue, 0);
+
+  return sum;
 };
 
 const multiply = function() {
-  const numbers = [];
-	for (let i = 0; i < arguments.length; i++) {
-    numbers.push(arguments[i]);
-  }
+  const numbers = arguments[0];
+
+  const multiply = numbers.reduce((previousValue, currentValue) => 
+  previousValue * currentValue, 1);
+
+  return multiply;
 };
 
-const power = function() {
-  const numbers = [];
-	for (let i = 0; i < arguments.length; i++) {
-    numbers.push(arguments[i]);
-  }
+const power = function(a, b) {
+  let power = a**b;
+  return power;
 };
 
-const factorial = function() {
-  const numbers = [];
-	for (let i = 0; i < arguments.length; i++) {
-    numbers.push(arguments[i]);
+const factorial = function(a) {
+  let factorial = 1;
+  for (let i = 1; i <= a; i++) {
+    factorial = factorial * i;
   }
+
+  return factorial;
 };
 
 // Do not edit below this line
