53590beaf78e09a48337b23f92f9211cac11d406
EHoldener <eholdener12@gmail.com>
2022-03-07 23:28:33 -0800

    started working on 08 calculator, its late, going to bed. Passed first 4 tests now

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
 const add = function() {
-	
+  const numbers = [];
+	for (let i = 0; i < arguments.length; i++) {
+    numbers.push(arguments[i]);
+  }
+  const sum = numbers.reduce((partialSum, a) => partialSum + a, 0);
+  return sum;
 };
 
 const subtract = function() {
-	
+  const numbers = [];
+	for (let i = 0; i < arguments.length; i++) {
+    numbers.push(arguments[i]);
+  }
+
+  const subtract = numbers.reduce((a, b) => a - b);
+  return subtract;
 };
 
 const sum = function() {
-	
+  const numbers = [];
+	for (let i = 0; i < arguments.length; i++) {
+    numbers.push(arguments[i]);
+  }
 };
 
 const multiply = function() {
-
+  const numbers = [];
+	for (let i = 0; i < arguments.length; i++) {
+    numbers.push(arguments[i]);
+  }
 };
 
 const power = function() {
-	
+  const numbers = [];
+	for (let i = 0; i < arguments.length; i++) {
+    numbers.push(arguments[i]);
+  }
 };
 
 const factorial = function() {
-	
+  const numbers = [];
+	for (let i = 0; i < arguments.length; i++) {
+    numbers.push(arguments[i]);
+  }
 };
 
 // Do not edit below this line
